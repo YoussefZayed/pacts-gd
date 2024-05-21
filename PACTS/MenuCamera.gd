5d3cead24bd562517b726ac00abf2ee95bf74070
Haydn Brown <85188181+HaydnBrown-Guardian@users.noreply.github.com>
2024-05-21 18:04:24 +0100

    Broke and then fixed zooming, now with 2x zoommier zoom

--- PACTS/MenuCamera.gd
+++ PACTS/MenuCamera.gd
@@ -1,10 +1,2 @@
 extends Camera2D
-@export var ZOOM_MIN = 5 # maximum zoom
 
-var imageX = 2500 # map dimensions x
-var imageY = 1428 # map dimensions y
-
-func _process(delta):
-	ZOOM_MIN = max(float(get_viewport().size.x)/imageX,float(get_viewport().size.y)/imageY) # set minimum zoom based on ratio of map size to viewport size
-	zoom.x = ZOOM_MIN
-	zoom.y = ZOOM_MIN
--- PACTS/UI/Camera.gd
+++ PACTS/UI/Camera.gd
@@ -19,12 +19,15 @@ var zoomFactor = 1.0 # variable to store how much the game should zoom in/out
 var zoomPos  =Vector2()
 var zooming = false # variable to track whether the user wants to zoom
 var mousePos = Vector2()
+var current_SPEEDMAX = 0
 
 func _ready():
 	@warning_ignore("integer_division") # ignore warning
 	position.x = mapX/2 # set inital position as the middle of the map x
 	@warning_ignore("integer_division") # ignore warning
 	position.y = mapY/2 # set inital position as the middle of the map y
+	ZOOM_MIN = min(1920/float(mapX),1080/float(mapY)) # set minimum zoom based on ratio of map size to viewport size
+	print(ZOOM_MIN)
 	zoom.x = ZOOM_MIN
 	zoom.y = ZOOM_MIN
 
@@ -32,37 +35,40 @@ func _process(delta):
 	var inputX = int((Input.is_action_pressed("ui_right") or Input.is_action_pressed("PressD"))) - int(Input.is_action_pressed("ui_left")or Input.is_action_pressed("PressA")) # get input x (left/right)
 	var inputY = int(Input.is_action_pressed("ui_down") or Input.is_action_pressed("PressS")) - int(Input.is_action_pressed("ui_up") or Input.is_action_pressed("PressW")) # get input y (up/down)
 	
-	ZOOM_MIN = min(float(get_viewport().size.x)/mapX,float(get_viewport().size.y)/mapY) # set minimum zoom based on ratio of map size to viewport size
 	ZOOM_MAX = max((mapX/float(get_viewport().size.x))/2,(mapY/float(get_viewport().size.y))/2) # set maximum zoom based on ratio of map size to viewport size
+	
 	zoom.x = lerp(zoom.x, zoom.x*zoomFactor, ZOOM_SPEED*delta) # set zoom x
 	zoom.y = lerp(zoom.y, zoom.y*zoomFactor, ZOOM_SPEED*delta) # set zoom y
 	zoom.x = clamp(zoom.x, ZOOM_MIN, ZOOM_MAX) # keep zoom within calculated limits
 	zoom.y = clamp(zoom.y, ZOOM_MIN, ZOOM_MAX) # keep zoom within calculated limits
 	$"../UI/Corner Margin/GridContainer/Label Margin/Zoom Label".text = str("Zoom: ", round(zoom.x*100)/100, "x")
 	
+	
 	if not zooming: # if not zooming
 		zoomFactor = 1.0 # don't change zoom
 	
+	current_SPEEDMAX = SPEEDMAX/zoom.x
+	
 	#Set Camera X Position
-	var targetX = int(lerp(position.x, position.x + inputX*XSPEED*sqrt(zoom.x), XSPEED*delta)) # set target x position
+	var targetX = int(lerp(position.x, position.x + inputX*XSPEED/sqrt(zoom.x), XSPEED*delta)) # set target x position
 	if inputX != 0: # check to see if button is being pressed
-		xspeedTarget = min(xspeedTarget+0.1,SPEEDMAX) # if button is being held accelerate
+		xspeedTarget = min(xspeedTarget+0.1,current_SPEEDMAX) # if button is being held accelerate
 	else:
 		xspeedTarget = max(xspeedTarget/2,SPEEDMIN) # if button is not being held decay
 	XSPEED = xspeedTarget*(1+0.5*int(Input.is_key_pressed(KEY_SHIFT))) # shift to camera sprint
 	
-	position.x = clamp(targetX, (get_viewport().size.x/2)/zoom.x, mapX-(get_viewport().size.x/2)/zoom.x) # push target position to display
+	position.x = clamp(targetX, (1920/2)/zoom.x, mapX-(1920/2)/zoom.x) # push target position to display
 	
 	#Set Camera Y Position
-	var targetY = int(lerp(position.y, position.y + inputY*YSPEED*sqrt(zoom.y), YSPEED*delta)) # set target y position
+	var targetY = int(lerp(position.y, position.y + inputY*YSPEED/sqrt(zoom.y), YSPEED*delta)) # set target y position
 	if inputY != 0: # check to see if button is being pressed
-		yspeedTarget = min(yspeedTarget+0.1,SPEEDMAX) # if button is being held accelerate
+		yspeedTarget = min(yspeedTarget+0.1,current_SPEEDMAX) # if button is being held accelerate
 	else:
 		yspeedTarget = max(yspeedTarget/2,SPEEDMIN) # if button is not being held decay
 	YSPEED = yspeedTarget*(1+0.5*int(Input.is_key_pressed(KEY_SHIFT))) # shift to camera sprint
 	
-	position.y = clamp(targetY, (get_viewport().size.y/2)/zoom.y, mapY-(get_viewport().size.y/2)/zoom.y) # push target position to display
-
+	position.y = clamp(targetY, (1080/2)/zoom.y, mapY-(1080/2)/zoom.y) # push target position to display
+	
 func _input(event):
 	if abs(zoomPos.x-get_global_mouse_position().x)>ZOOM_MARGIN:
 		zoomFactor = 1.0
